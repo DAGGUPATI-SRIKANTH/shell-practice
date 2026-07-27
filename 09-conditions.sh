504f26cc72a8af3c07ae80d13fa11718aa1c7d4e
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-07-27 05:07:28 -0500

    actions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -11,6 +11,8 @@ NUM=$1
 
 if [ $NUM -gt 20 ]; then
     echo "$NUM is greater than 20"
+elif [ $NUM -eq 20 ]; then
+    echo "$NUM is equal to 20"
 else
   echo "$NUM is less than or equal to 20"
 fi
