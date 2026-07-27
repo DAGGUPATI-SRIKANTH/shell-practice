ce60c7655e99be17d5fe55d990b8e3e788c91d37
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-07-27 01:56:35 -0500

    actions

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Suresh # No space between value and =
+PERSON2=Ramesh
+
+echo "$PERSON1:: hello $PERSON2, how are you doing?"
+echo "$PERSON2:: I am doing good, How about you?"
+echo "$PERSON1:: I am doing good too,what are you doing these days?"
+echo "$PERSON2:: I am learning shell scripting, it is very interesting"
