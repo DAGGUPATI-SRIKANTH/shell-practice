bb3cbe773039057fa79c9b1477907bd8283a392d
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-07-27 05:02:26 -0500

    actions

---
+++ 09-conditions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUM=25
+
+# -gt is Greater than
+# -lt is less than
+# -eq is equal to
+# -ne is not equal to
+# -ge is greater than or equal to
+# -le is less than or equal to
+
+if [ $NUM -gt 20 ]; then
+    echo "$NUM is greater than 20"
+fi
