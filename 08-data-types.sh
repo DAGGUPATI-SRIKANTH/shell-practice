f855bbba241d9840762c890bf54d288fe1946e08
DAGGUPATI-SRIKANTH <daggupatisrikanth35@gmail.com>
2026-07-27 04:44:55 -0500

    actions

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -5,3 +5,10 @@ num2=200
 
 SUM=$((num1+num2))
 echo "Sum is: $SUM"
+
+FRUITS=("APPPLE" "BANANA" "MANGO" "ORANGE")
+
+echo "First fruit is: ${FRUITS[0]}"
+echo "Second fruit is: ${FRUITS[1]}"
+echo "Third fruit is: ${FRUITS[2]}"
+echo "Fourth fruit is: ${FRUITS[3]}"
